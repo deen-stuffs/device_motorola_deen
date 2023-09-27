c95c38af7a7e88966f3b8d134084a6a428f260fb
Vhmit <viktor.9630@hotmail.com>
2023-09-26 23:50:16 -0300

    deen: prebuilt: Import Via Browser
    * To replace standard browsers like jelly
    
    Change-Id: Ib862d5721e52f74da4c547ce6e115f1b50430270
    Signed-off-by: Vhmit <viktor.9630@hotmail.com>

--- device.mk
+++ device.mk
@@ -310,6 +310,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
+# Packages
+PRODUCT_PACKAGES += \
+    Via
+
 # Perf
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
