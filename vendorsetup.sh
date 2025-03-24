89fd1192695c442b40c3fd60598e520205325c42
Victor Hugo <praxedes.9630@gmail.com>
2025-03-24 12:42:58 -0300

    deen: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,15 +5,3 @@ git clone -b lineage-18.1 https://github.com/LineageOS/android_external_bson ext
 git clone -b lineage-18.1 https://github.com/LineageOS/android_system_qcom system/qcom
 git clone -b lineage-18.1 https://github.com/LineageOS/android_hardware_motorola hardware/motorola
 echo ""
-
-echo -e "\033[33mDeseja aplicar os patches do ih8sn? (s/n)\033[0m"
-read resposta
-
-if [ "$resposta" = "s" ]; then
-echo -e "\033[32mInicializando...\033[0m"
-git clone -b master https://github.com/Vhmit/ih8sn ih8sn
-./ih8sn/patches/eleven/patch.sh
-echo -e "\033[32m Tudo no seu devido lugar, Boa compilação!!\033[0m"
-else
-echo -e "\033[31mAs dependências foram puladas!!\033[0m"
-fi
